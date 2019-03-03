d5df8b6de6dd407ccdcbf998a0045ec12f1b5145
Andrew Bates <andrewbates73@gmail.com>
2019-03-03 13:27:34 -0800

    ames: log outcome and remove var that gets NaN'ed

--- data/make-ames.R
+++ data/make-ames.R
@@ -5,26 +5,36 @@ library(recipes)
 
 ames <- make_ames()
 
-ames
-str(ames)
-names(ames)
 
 # create a simplified version with only numeric columns
 ames_numeric <- ames %>% 
   select_if(is.numeric)
 
-write_csv(ames_numeric, "data/ames-numeric.csv")
+ames_num_rec <- recipe(Sale_Price ~., data = ames_numeric) %>% 
+  step_center(all_predictors()) %>% 
+  step_scale(all_predictors()) %>% 
+  step_log(all_outcomes())
+
+ames_numeric_processed <- ames_num_rec %>% 
+  prep() %>% 
+  bake(new_data = ames_numeric) %>% 
+  rename(Log_Sale_Price = Sale_Price)
+
+write_csv(ames_numeric_processed, "data/ames-numeric.csv")
 
 
 # make a processed version of the entire data set
 ames_rec <- recipe(Sale_Price ~., data = ames) %>% 
   step_dummy(all_nominal()) %>% 
   step_center(all_predictors()) %>% 
-  step_scale(all_predictors())
+  step_scale(all_predictors()) %>% 
+  step_log(all_outcomes())
 
 ames_processed <- ames_rec %>% 
   prep() %>% 
-  bake(new_data = ames)
+  bake(new_data = ames) %>% 
+  rename(Log_Sale_Price = Sale_Price) %>% 
+  select(-Overall_Cond_Very_Excellent) # gets all set to NaN
 
 write_csv(ames_processed, "data/ames-processed.csv")
 
