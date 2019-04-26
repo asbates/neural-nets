864bb0eade99d4088326f47ee46d658f7054801b
Andrew Bates <andrewbates73@gmail.com>
2019-04-26 19:57:30 -0400

    fine tune last vgg16 weights

--- not-hotdog/pretrained-data-aug.R
+++ not-hotdog/pretrained-data-aug.R
@@ -141,16 +141,66 @@ save_model_hdf5(one_dense256,
                 "~/Desktop/hotdog_models/2019-04-26-vgg16-256drop.h5")
 
 
+# =============================
+# ===== fine tuning ===========
+# =============================
 
+# fine tune the best model so far as in 5.3.2
+# we'll retrain from block 4
+# note: i orignally saved the 3 models above
+# then was going to load them and unfreeze weights in the conv base
+# but it looks like the conv base part of the model after loading
+# is all one big thing
+# so model above was retrained before the following was run
 
+unfreeze_weights(conv_base, from = "block4_conv1")
 
+one_dense256 %>% 
+  compile(
+    loss = "binary_crossentropy",
+    optimizer = optimizer_rmsprop(lr = 1e-5),
+    metrics = c("acc")
+  )
 
+one_dense256ft_hist <- one_dense256 %>% 
+  fit_generator(
+    train_generator,
+    steps_per_epoch = 40,
+    epochs = 50,
+    validation_data = val_generator,
+    validation_steps = 10
+  )
 
+# 90% validation accuracy. not really much better than w/out fine tuning
 
+plot(one_dense256ft_hist)
 
+# maybe this will help with the optimizer problem had before
+freeze_weights(one_dense256)
 
+save_model_hdf5(one_dense256,
+                "~/Desktop/hotdog_models/2019-04-26-vgg16-fine-tune.h5")
 
+# evaluate on test set
+test_generator <- flow_images_from_directory(
+  test_dir,
+  test_datagen,
+  target_size = c(150, 150),
+  batch_size = 2,
+  class_mode = "binary"
+)
 
+one_dense256 %>% 
+  evaluate_generator(
+    test_generator,
+    steps = 49
+  )
 
+# $loss
+# [1] 0.4292491
+# 
+# $acc
+# [1] 0.8979592
 
+# i'll take it. MUCH better than before
 
